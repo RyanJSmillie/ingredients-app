25212efaca465cedafe70ef196076c142cac34dd
Danielle Davis <danielledavis@danielles-mbp.home>
2022-08-15 20:29:03 +0100

    merge with back end

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const app = require('./src/app.js');
+const app = require("./src/app.js");
 
-const APP_PORT = 5000;
+const APP_PORT = 3306;
 
 app.listen(APP_PORT, () => {
-  console.log(`App is listening on port ${APP_PORT}`)
-})
+  console.log(`App is listening on port ${APP_PORT}`);
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,23 +6,23 @@ const app = express();
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "ingredients_library_mysql"
+  port: 3308,
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "ingredients_library_mysql",
 });
 
-db.connect ( (error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected...")
-    }
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MYSQL Connected...");
+  }
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello User!")
+app.get("/", (req, res) => {
+  res.send("Hello User!");
 });
-
 
 module.exports = app;
