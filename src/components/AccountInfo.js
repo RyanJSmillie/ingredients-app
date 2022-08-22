3a7c5b480e1fbb23a12b0022f28fd9794074aae4
Danielle Davis <danielledavis@danielles-mbp.home>
2022-08-22 17:58:49 +0100

    styling and update to account info

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,13 +1,15 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { React, useState } from "react";
-import { Link } from "react-router-dom";
-import SignIn from "./SignIn";
+import "../styles/accountInfo.css";
+// import { Link } from "react-router-dom";
+// import SignIn from "./SignIn";
 
 function AccountInfo() {
   const [accountDetails, setAccountDetails] = useState({
     username: "DanielleD8134",
     emailAddress: "test@test.com",
     password: "password",
+    password2: "password2",
   });
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -36,52 +38,69 @@ function AccountInfo() {
   //   }
 
   return (
-    <div>
-      <p>Account Info</p>
-      <div className="form-content-right">
-        <form className="form" onSubmit={onFormSubmit}>
-          <h1>Account Information</h1>
-          <div className="form-inputs">
-            <label htmlFor="username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={accountDetails.username}
-              onChange={onChange}
-              className="form-input"
-              placeholder="Enter your username"
-            />
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="form-content-right">
+            <form className="form" onSubmit={onFormSubmit}>
+              <h1>Account Information</h1>
+
+              <div className="form-inputs">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  value={accountDetails.username}
+                  onChange={onChange}
+                  className="form-input"
+                  placeholder="Enter your username"
+                />
+              </div>
+              <div className="form-inputs">
+                <label htmlFor="emailAddress" className="form-label">
+                  Email Address
+                </label>
+                <input
+                  type="text"
+                  name="emailAddress"
+                  value={accountDetails.emailAddress}
+                  onChange={onChange}
+                  className="form-input"
+                  placeholder="Enter your email address"
+                />
+              </div>
+              <div className="form-inputs">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={accountDetails.password}
+                  onChange={onChange}
+                  className="form-input"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div className="form-inputs">
+                <label htmlFor="password2" className="form-label">
+                  Confirm Password
+                </label>
+                <input
+                  type="password2"
+                  name="password2"
+                  value={accountDetails.password2}
+                  onChange={onChange}
+                  className="form-input"
+                  placeholder="Confirm your password"
+                />
+              </div>
+              <button type="submit">Update</button>
+            </form>
           </div>
-          <div className="form-inputs">
-            <label htmlFor="emailAddress" className="form-label">
-              Email Address
-            </label>
-            <input
-              type="text"
-              name="EmailAddress"
-              value={accountDetails.emailAddress}
-              onChange={onChange}
-              className="form-input"
-              placeholder="Enter your email address"
-            />
-          </div>
-          <div className="form-inputs">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={accountDetails.password}
-              onChange={onChange}
-              className="form-input"
-              placeholder="Enter your password"
-            />
-          </div>
-          <button type="submit">Update</button>
-        </form>
+        </div>
       </div>
     </div>
   );
